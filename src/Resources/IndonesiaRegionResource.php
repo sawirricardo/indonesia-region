15a68606c762c7fe919b39be1bbe6e8f96c25fb0
sawirricardo <sawir.ricardo@gmail.com>
2022-02-10 20:59:27 +0700

    Added Resources

---
+++ src/Resources/IndonesiaRegionResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sawirricardo\IndonesiaRegion\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class IndonesiaRegionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
